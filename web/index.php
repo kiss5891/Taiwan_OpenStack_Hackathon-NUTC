aa95e0320163a4df6594e3ea8cdc994a303213de
Ci-Jie <cjlee8110@gmail.com>
2016-03-19 14:27:45 +0800

    update web folder name

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-	if(isset($_GET['x'])&&isset($_GET['y'])){
-		$xml = file_get_contents("http://root:r00tme@172.16.2.26/axis-cgi/com/ptz.cgi?info=1&camera=1&center=".$_GET['x'].",".$_GET['y']);
-		echo "Success!!";
-	}
+  if(isset($_GET['x'])&&isset($_GET['y'])){
+    $xml = file_get_contents("http://root:r00tme@172.16.2.26/axis-cgi/com/ptz.cgi?info=1&camera=1&center=".$_GET['x'].",".$_GET['y']);
+    echo "Success!!";
+  }
